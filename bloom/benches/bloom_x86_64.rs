84ae678b4ac0552ef09d63a00985bb322551a0bf
marc <marcinbieszke@gmail.com>
2025-04-30 07:02:00 +0200

    remove obsolete bench / add bench add_hash_atomic

--- bloom/benches/bloom_x86_64.rs
+++ bloom/benches/bloom_x86_64.rs
@@ -9,22 +9,22 @@ use {
     test::Bencher,
 };
 
-#[cfg(target_arch = "x86_64")]
 #[bench]
-fn bench_contains_popcnt64(bencher: &mut Bencher) {
+fn bench_add_hash_atomic(bencher: &mut Bencher) {
     let mut rng = rand::thread_rng();
     let hash_values: Vec<_> = std::iter::repeat_with(Hash::new_unique)
         .take(1200)
         .collect();
     let mut fail = 0;
-    let bloom: ConcurrentBloom<_> = Bloom::random(1287, 0.1, 7424).into();
-
-    // Add all values first
-    for hash_value in &hash_values {
-        bloom.add(hash_value);
-    }
-
     bencher.iter(|| {
+        let bloom: ConcurrentBloom<_> = Bloom::random(1287, 0.1, 7424).into();
+        // Intentionally not using parallelism here, so that this and above
+        // benchmark only compare the bit-vector ops.
+        // For benchmarking the parallel code, change bellow for loop to:
+        //     hash_values.par_iter().for_each(|v| bloom.add(v));
+        for hash_value in &hash_values {
+            bloom.add(hash_value);
+        }
         let index = rng.gen_range(0..hash_values.len());
         if !bloom.contains_popcnt64(&hash_values[index]) {
             fail += 1;
