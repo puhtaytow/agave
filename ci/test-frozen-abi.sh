4dc51ab35bb864b95d3aa5a68dd1855e24e3acf8
marc <marcinbieszke@gmail.com>
2025-12-31 09:45:36 +0100

    works?

--- ci/test-frozen-abi.sh
+++ ci/test-frozen-abi.sh
@@ -9,9 +9,16 @@ here=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # shellcheck source=ci/rust-version.sh
 source "$here/rust-version.sh" nightly
 
-packages=$(cargo +"$rust_nightly" metadata --no-deps --format-version=1 | jq -r '.packages[] | select(.features | has("frozen-abi")) | .name')
-for package in $packages; do
-  cmd="cargo +$rust_nightly test -p $package --features frozen-abi --lib -- test_abi_digest test_api_digest --nocapture"
+# packages=$(cargo +"$rust_nightly" metadata --no-deps --format-version=1 | jq -r '.packages[] | select(.features | has("frozen-abi")) | .name')
+# for package in $packages; do
+  cmd="cargo +$rust_nightly test -p solana-vote --features frozen-abi --lib -- test_abi_digest test_api_digest --nocapture"
   echo "--- $cmd"
   $cmd
-done
+# done
+
+# packages=$(cargo +"$rust_nightly" metadata --no-deps --format-version=1 | jq -r '.packages[] | select(.features | has("frozen-abi")) | .name')
+# for package in $packages; do
+#   cmd="cargo +$rust_nightly test -p $package --features frozen-abi --lib -- test_abi_digest test_api_digest --nocapture"
+#   echo "--- $cmd"
+#   $cmd
+# done
